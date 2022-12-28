2cebb0109d2870ed17f396c82c2de4da78b2d60e
faxad <nfv@.com>
2022-12-28 21:42:30 +0500

    first save

---
+++ main.js
@@ -0,0 +1,22 @@
+//console.log(10); -console.log() -Это способ вывода информации в консоль браузера
+
+
+var imya=prompt('Введите ваше имя')
+var myAgeIs=prompt('Введите ваш возраст')
+ var imya = 'fakhad'
+ var myAgeIs = 18
+
+
+ console.log('Я '+imya);
+ console.log('мне '+myAgeIs);
+
+var str = +prompt ('решите пример'+'25+5=')
+var str = +prompt ('решите пример'+'20-2=')
+var str = +prompt ('решите пример'+'30*4=')
+var str = +prompt ('решите пример'+'30/5=')
+
+ console.log('25+5='+(25+5));
+ console.log('20-2='+(20-2));
+ console.log('30*4='+30*4);
+ console.log('30/5='+30/5);
+
